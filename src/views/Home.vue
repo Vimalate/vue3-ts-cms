67d3ab93a9ac0f716872ba2d6cd55b2dab22b3e1
Vimalate <1902644369@qq.com>
2021-10-01 14:37:09 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+
+
+
+
+import { defineComponent } from "vue"
+import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
