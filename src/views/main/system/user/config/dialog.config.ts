8522f69b30bf300e22ea57c57edc7961ecaa540a
Vimalate <1902644369@qq.com>
2021-10-16 21:26:25 +0800

    用户管理

--- src/views/main/system/user/config/dialog.config.ts
+++ src/views/main/system/user/config/dialog.config.ts
@@ -1,44 +1,44 @@
-import { IZsForm } from '@/baseUi/zsForm/types'
+import { IZsForm } from "@/baseUi/zsForm/types"
 
 export const dialogConfig: IZsForm = {
   formItems: [
     {
-      field: 'name',
-      label: '用户名',
-      type: 'input',
-      placeholder: '请输入用户名'
+      field: "name",
+      label: "用户名",
+      type: "input",
+      placeholder: "请输入用户名"
     },
     {
-      field: 'realname',
-      label: '真实姓名',
-      type: 'input',
-      placeholder: '请输入真实姓名'
+      field: "realname",
+      label: "真实姓名",
+      type: "input",
+      placeholder: "请输入真实姓名"
     },
     {
-      field: 'password',
-      label: '密码',
-      type: 'input',
-      placeholder: '请输入密码',
+      field: "password",
+      label: "密码",
+      type: "input",
+      placeholder: "请输入密码",
       isHidden: true
     },
     {
-      field: 'cellphone',
-      label: '电话号码',
-      type: 'input',
-      placeholder: '请输入电话号码'
+      field: "cellphone",
+      label: "电话号码",
+      type: "input",
+      placeholder: "请输入电话号码"
     },
     {
-      field: 'departmentId',
-      type: 'select',
-      label: '选择部门',
-      placeholder: '请选择部门',
+      field: "departmentId",
+      type: "select",
+      label: "选择部门",
+      placeholder: "请选择部门",
       options: []
     },
     {
-      field: 'roleId',
-      type: 'select',
-      label: '选择角色',
-      placeholder: '请选择角色',
+      field: "roleId",
+      type: "select",
+      label: "选择角色",
+      placeholder: "请选择角色",
       options: []
     }
   ],
